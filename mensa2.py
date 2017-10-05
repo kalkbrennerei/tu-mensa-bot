fd3c4261957dd57e4225165313eaae463440ad50
Lydia Charlotte Maite Kalkbrenner <lydia.kalkbrenner@campus.tu-berlin.de>
2017-10-05 17:56:16 +0200

    user-agent changed

--- mensa2.py
+++ mensa2.py
@@ -4,22 +4,25 @@ from lxml import html
 def food():
 
 	# specify user-agent
-	headers = {'User-Agent': 'curl/7.43.0'}
+	headers = {'User-Agent': 'https://github.com/lydl28/tu-mensa-bot'}
 
-	type_path = '//div[@class="container-fluid splGroupWrapper"][{}]//div[@class="col-md-12 splGroup"]/text()'
-	entry_path = '//div[@class="container-fluid splGroupWrapper"][{}]//span/text()'
-
+	# make request and check status code
 	page = requests.get('https://www.stw.berlin/mensen/mensa-tu-hardenbergstra%C3%9Fe.html', headers=headers)
+	if(page.status_code != 200):
+		return None
+
 	page = page.content
 	tree = html.fromstring(page)
 
+	# define path
+	type_path = '//div[@class="container-fluid splGroupWrapper"][{}]//div[@class="col-md-12 splGroup"]/text()'
+	entry_path = '//div[@class="container-fluid splGroupWrapper"][{}]//span/text()'
+
 	# define food dict
 	food = {}
-	counter = 1
-	while(counter < 8):
-		typ = tree.xpath(type_path.format(str(counter)))[0]
-		entries = tree.xpath(entry_path.format(str(counter)))
-		food[typ] = entries
-		counter+=1
+	for n in range (1,8):
+		type = tree.xpath(type_path.format(str(n)))[0]
+		entries = tree.xpath(entry_path.format(str(n)))
+		food[type] = entries
 
 	return food
